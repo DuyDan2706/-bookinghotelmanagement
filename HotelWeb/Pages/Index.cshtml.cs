87317b7bcf521628842b82875bdcc90060004cb3
donework-now <huynhanhkiet2222@gmail.com>
2022-11-13 09:44:02 +0700

    update Room Management(01)

--- HotelWeb/Pages/Index.cshtml.cs
+++ HotelWeb/Pages/Index.cshtml.cs
@@ -12,17 +12,30 @@ namespace HotelWeb.Pages
 {
     public class IndexModel : PageModel
     {
-      
-     
+
+        private readonly IRoomRepository roomRepository;
+        private readonly IRoomTypeRepository roomTypeRepository;
+
+        public IndexModel(IRoomRepository _roomRepository, IRoomTypeRepository _roomTypeRepository)
+        {
+            roomRepository = _roomRepository;
+            roomTypeRepository = _roomTypeRepository;
+
+
+        }
         [BindProperty]
         public Room Room { get; set; }
         public IList<Room> RoomList { get; set; }
         public IList<RoomType> RoomTypeList { get; set; }
         public IActionResult OnGet()
-        {           
+        {
             try
             {
-              
+                RoomList = roomRepository.GetRooms().ToList();
+                RoomList = RoomList.Where(p => p.Active == true).OrderBy(d => d.Id).Take(6).ToList();
+
+                RoomTypeList = roomTypeRepository.GetRoomsType().ToList();
+                RoomTypeList = RoomTypeList.Where(p => p.Active == true).ToList();
             }
             catch (Exception ex)
             {
